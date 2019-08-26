966cce710a190ac60905e6d1ea57b072eae74ece
bensalem hajer <hajer.bensalem.2016@gmail.com>
2019-08-26 08:24:45 +0100

    Ajoutez des fichiers projet.

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,56 @@
+﻿using MediatR;
+using MicroRabbit.GestionFournisseur.Application.Interfaces;
+using MicroRabbit.GestionFournisseur.Application.Services;
+using MicroRabbit.GestionFournisseur.Data.Context;
+using MicroRabbit.GestionFournisseur.Data.Repository;
+using MicroRabbit.GestionFournisseur.Domain.CommandHandlers;
+using MicroRabbit.GestionFournisseur.Domain.Commands;
+using MicroRabbit.GestionFournisseur.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using MicroRabbit.GestionCompresseur.Application.Interfaces;
+using MicroRabbit.GestionCompresseur.Application.Services;
+using MicroRabbit.GestionCompresseur.Data.Context;
+using MicroRabbit.GestionCompresseur.Data.Repository;
+using MicroRabbit.GestionCompresseur.Domain.EventHandlers;
+using MicroRabbit.GestionCompresseur.Domain.Events;
+using MicroRabbit.GestionCompresseur.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddSingleton<IEventBus, RabbitMQBus>(sp =>
+            {
+                var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+                return new RabbitMQBus(sp.GetService<IMediator>(), scopeFactory);
+            });
+
+            //Subscriptions
+            services.AddTransient<CreationEventHandler>();
+
+            //Domain Events
+            services.AddTransient<IEventHandler<CreationDoneEvent>, CreationEventHandler>();
+
+            //Domain Banking Commands
+            services.AddTransient<IRequestHandler<CreateFournisseurCommand, bool>, FournisseurCommandHandler>();
+
+            //Application Services
+            services.AddTransient<IFournisseurService, FournisseurService>();
+            services.AddTransient<ICompresseurService, CompresseurService>();
+
+            //Data
+            services.AddTransient<IFournisseurRepository, FournisseurRepository>();
+            services.AddTransient<ICompresseurRepository, CompresseurRepository>();
+            services.AddTransient<FournisseurDbContext>();
+            services.AddTransient<CompresseurDbContext>();
+        }
+    }
+}
